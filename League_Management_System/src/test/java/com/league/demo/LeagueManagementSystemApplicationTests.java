4cf229d85daa7e2bd256f5555006b581421b6c2d
avikdasm99 <110466059+avikdasm99@users.noreply.github.com>
2023-01-13 16:44:33 +0530

    Add files via upload

---
+++ League_Management_System/src/test/java/com/league/demo/LeagueManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.league.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeagueManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
