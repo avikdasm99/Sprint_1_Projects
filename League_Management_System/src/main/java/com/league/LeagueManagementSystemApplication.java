e0bc680578b673ee8b4cadcc4ef37cd5d52511e4
avikdasm99 <110466059+avikdasm99@users.noreply.github.com>
2023-01-13 16:26:51 +0530

    Add files via upload

---
+++ League_Management_System/src/main/java/com/league/LeagueManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.league;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeagueManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeagueManagementSystemApplication.class, args);
+	}
+
+}
